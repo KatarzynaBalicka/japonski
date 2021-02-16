08be938442bfa052e750742a977cec04b2dcd659
Katarzyna Balicka <kbbali@gmail.com>
2021-02-16 11:40:33 +0000

    exercise template and grammar cleaning up

--- jap_main.js
+++ jap_main.js
@@ -5,3 +5,17 @@ hamburger.addEventListener('click', () => {
     const navigation = document.querySelector('.navigation_top--js');
     navigation.classList.toggle('navigation--open')
 })
+
+const menuBtn = document.querySelector('.menu-ham');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    }
+    else {
+        menuBtn.classList.remove('open')
+    menuOpen= false;
+    
+    }
+})
