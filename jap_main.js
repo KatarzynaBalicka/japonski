5949caf450179b17cc8b8849179ad17ad0ac7aa4
Katarzyna Balicka <kbbali@gmail.com>
2021-02-19 14:45:16 +0000

    adjusting bottoms margins

--- jap_main.js
+++ jap_main.js
@@ -6,16 +6,16 @@ hamburger.addEventListener('click', () => {
     navigation.classList.toggle('navigation--open')
 })
 
-const menuBtn = document.querySelector('.menu-ham');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    }
-    else {
-        menuBtn.classList.remove('open')
-    menuOpen= false;
+// const menuBtn = document.querySelector('.menu-ham');
+// let menuOpen = false;
+// menuBtn.addEventListener('click', () => {
+//     if(!menuOpen) {
+//         menuBtn.classList.add('open');
+//         menuOpen = true;
+//     }
+//     else {
+//         menuBtn.classList.remove('open')
+//     menuOpen= false;
     
-    }
-})
+//     }
+// })
